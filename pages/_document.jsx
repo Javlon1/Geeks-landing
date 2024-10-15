aca3aee2d876d4ce31b4feaa722af3d5cc57976e
javlon1 <muhamedmijotot@gmail.com>
2024-10-15 15:55:41 +0500

    font

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,6 +13,11 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet" />
+
                 </Head>
                 <body>
                     <Main />
